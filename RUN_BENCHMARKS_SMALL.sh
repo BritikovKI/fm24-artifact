23481e6942b29c8499508aa42a52d5c93c8e77f7
BritikovKI <BritikovKI@yandex.ru>
2024-06-25 08:48:47 +0200

    10 secs exec

--- RUN_BENCHMARKS_SMALL.sh
+++ RUN_BENCHMARKS_SMALL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 
 echo Running SAT instances
-./docker_run_multiloop_sat_parallel.sh -t 20
+./docker_run_multiloop_sat_parallel.sh -t 10
 
 echo Running UNSAT instances
-./docker_run_multiloop_unsat_parallel.sh -t 20
+./docker_run_multiloop_unsat_parallel.sh -t 10
 
 echo Drawing plot
 ./host_scripts/draw_plot_small.sh
